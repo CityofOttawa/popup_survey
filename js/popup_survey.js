58ec6b6272931ba35ed28c8af1d04eb927117665
unknown <dave@baglerit.com>
2013-06-14 14:14:39 -0400

    Added anonymous user check to the module file and added the create/read cookie functionality to the javascript file.

--- js/popup_survey.js
+++ js/popup_survey.js
@@ -7,6 +7,15 @@
 
   $(document).ready(function() {
     centrePopup();
+    
+    var cookie = readCookie('ottcity_site_survey');
+
+    if (!cookie) {
+      createCookie('ottcity_site_survey','isActive', 14);
+    }
+    else {
+      $('.bean-survey-popup-message').hide();
+    }
   
     $(window).resize(function() {
       centrePopup();  
@@ -51,5 +60,27 @@
     
     return popUnderWin;
   }
+  
+  function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      expires = "; expires="+date.toGMTString();
+    }
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+  function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+
   
 })(jQuery, Drupal, this, this.document);
