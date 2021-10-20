eb3400e8ecbb84d9afd6ad93fc1ca1eaaf70d99f
François Xavier Lemieux <flemieux@coldfrontlabs.ca>
2021-10-20 16:15:45 -0400

    feat(survey): add global popup survey config form, create template and theme hooks, update js

--- js/popup_survey.js
+++ js/popup_survey.js
@@ -2,102 +2,21 @@
 // wrapping it with an "anonymous closure". See:
 // - http://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
-(function ($, Drupal, window, document, undefined) {
+(function ($, Drupal, window, document, drupalSettings) {
 
   $(document).ready(function() {
-    centrePopup();
-    focusOnPopup();
-
-    var bots = new RegExp(Drupal.settings.POPUP_SURVEY.botlist, "i");
+    var bots = new RegExp(drupalSettings.popup_survey.botlist, "i");
     if( navigator.userAgent && bots.test(navigator.userAgent) ) {
       return;
     }
 
     var cookie = readCookie('ottcity_site_survey');
-
     if(!cookie) {
       createCookie('ottcity_site_survey','isActive', 14);
-      $('.popup-greyout').addClass('cover');
-      $('.bean-survey-popup-message').show();
-    }
-    else {
-      $('.bean-survey-popup-message').hide();
-      $('.popup-greyout').hide();
     }
 
-    $(window).resize(function() {
-      centrePopup();
-    });
-
-    $('#popup-reject').click(function(event) {
-      event.preventDefault();
-      $('.bean-survey-popup-message').hide();
-      $('.popup-greyout').hide();
-    });
-
-    $('#popup-accept').click(function(event) {
-      event.preventDefault();
-      surveyWindow = popUnder($(this).attr('href'));
-      $('.bean-survey-popup-message').hide();
-      $('.popup-greyout').hide();
-    });
-
+    $('#popupSurveyModal').modal('show');
   });
-
-  function focusOnPopup() {
-    var $popup = $('.bean-survey-popup-message');
-    var $accept = $('#popup-accept');
-    if(1 <= $popup.length && 1 <= $accept.length) {
-      $popup.attr('tabindex', -1);
-      $popup.focus();
-      // Modal Trap
-      // Tabbing past the accept button takes you to the top of the modal.
-      $accept.on('keydown', function (e) {
-        if ((e.which === 9 && !e.shiftKey)) {
-          e.preventDefault();
-          $popup.focus();
-        }
-      });
-      // Shift tabbing back past the modal takes you to the accept button of the modal.
-      $popup.on('keydown', function (e) {
-        if ((e.which === 9 && e.shiftKey)) {
-          e.preventDefault();
-          $accept.focus();
-        }
-      });
-
-    }
-  }
-
-  function centrePopup() {
-    var $popup = $('.bean-survey-popup-message');
-    if(1 <= $popup.length) {
-      $popup.css("margin-top", -$popup.height() / 2 + "px");
-      $popup.css("margin-left", -$popup.width() / 2 + "px");
-      $popup.css("visibility", "visible");
-    }
-  }
-
-  function popUnder(url) {
-    var popUnderWin;
-    var nav = navigator.userAgent;
-    var isGecko = /rv:[2-9]/.exec(nav);
-    var hackString = nav.indexOf('Chrome') > -1 ? "scrollbar=yes" : "toolbar=1,statusbar=1,resizable=1,scrollbars=1,menubar=1,location=1,directories=0";
-
-    popUnderWin = window.open("about:blank", "title", hackString); //+ ",height=" + height + ",width=" + width
-
-    if (isGecko) {
-      popUnderWin.window.open("about:blank").close();
-    }
-
-    popUnderWin.document.location.href = url;
-
-    setTimeout(window.focus);
-    window.focus();
-    popUnderWin.blur();
-
-    return popUnderWin;
-  }
 
   function createCookie(name,value,days) {
     var expires = "";
@@ -121,4 +40,4 @@
   }
 
 
-})(jQuery, Drupal, this, this.document);
+})(jQuery, Drupal, this, this.document, drupalSettings);
---
+++ src/Form/PopupSurveySettingsForm.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Drupal\popup_survey\Form;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configure popup_survey settings for this site.
+ */
+class PopupSurveySettingsForm extends ConfigFormBase {
+  /**
+   * Shows the popup on every page except the listed pages.
+   */
+  const POPUP_VISIBILITY_NOTLISTED = 0;
+
+  /**
+   * Shows the popup on only the listed pages.
+   */
+  const POPUP_VISIBILITY_LISTED = 1;
+
+  /**
+   * Config settings.
+   *
+   * @var string
+   */
+  const SETTINGS = 'popup_survey.settings';
+
+  /**
+   * @var EntityTypeManagerInterface $entity_manager
+   */
+  protected $entity_manager;
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_manager) {
+    $this->entity_manager = $entity_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      // Load the service required to construct this class.
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'popup_survey_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      static::SETTINGS,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config(static::SETTINGS);
+
+    $config_entity_default = ($config->get('popup_survey_config_entity')) ?: 0;
+    $form['popup_survey_config_entity'] = array(
+      '#type'        => 'select',
+      '#title'       => t('Popup Config Entity'),
+      '#description' => t('Select the PopupSurvey config entity that you would like to display to visitors as the survey invitation. If you wish not to display the popup select \'none\' for the popup block.'),
+      '#options'     => $this->popupSurveyEntityOptions(),
+      '#default_value' => $config_entity_default,
+      '#required' => TRUE,
+    );
+
+    $frequency_default = ($config->get('popup_survey_frequency')) ?: 1;
+    $form['popup_survey_frequency'] = array(
+      '#type'        => 'select',
+      '#title'       => t('Popup Frequency'),
+      '#description' => t('Display the popup for one out of every # visitors.'),
+      '#options'     => $this->popupSurveyFrequencyOptions(),
+      '#default_value' => $frequency_default,
+      '#required' => TRUE,
+    );
+
+    // Visibility settings.
+    $form['visibility_title'] = array(
+      '#type' => 'item',
+      '#title' => t('Visibility settings'),
+    );
+
+    $options = array(
+      self::POPUP_VISIBILITY_NOTLISTED => t('All pages except those listed'),
+      self::POPUP_VISIBILITY_LISTED => t('Only the listed pages'),
+    );
+
+    $ua_exclude_default = ($config->get('popup_survey_ua_exclude')) ?: 'alexa|bot|crawl|bing|facebookexternalhit|feedburner|google|preview|nagios|postrank|pingdom|slurp|spider|yahoo|yandex|sogou';
+    $form['popup_survey_ua_exclude'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Block blacklist'),
+      '#description' => t('User agents that match the list will not be presented with the survey popup.  Separate terms by a pipe (|) without spaces.  Do not enter line breaks.'),
+      '#default_value' => $ua_exclude_default,
+    );
+
+    $visibility_options_default = ($config->get('popup_survey_visibility_options')) ?: self::POPUP_VISIBILITY_NOTLISTED;
+    $form['popup_survey_visibility_options'] = array(
+      '#type' => 'radios',
+      '#title' => t('Show block on specific pages'),
+      '#options' => $options,
+      '#default_value' => $visibility_options_default
+    );
+
+    $visibility_options_pages_default = ($config->get('popup_survey_visibility_options_pages')) ?: '';
+    $form['popup_survey_visibility_options_pages'] = array(
+      '#type' => 'textarea',
+      '#title' => '<span class="element-invisible">' . t('Pages') . '</span>',
+      '#default_value' => $visibility_options_pages_default,
+      '#description' => t("Specify pages by using their paths. Enter one path per line. The '*' character is a wildcard. Example paths are %blog for the blog page and %blog-wildcard for every personal blog. %front is the front page.", array('%blog' => 'blog', '%blog-wildcard' => 'blog/*', '%front' => '<front>')),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Retrieve the configuration.
+    $this->configFactory->getEditable(static::SETTINGS)
+      // Set the submitted configuration setting.
+      ->set('popup_survey_config_entity', $form_state->getValue('popup_survey_config_entity'))
+      ->set('popup_survey_frequency', $form_state->getValue('popup_survey_frequency'))
+      ->set('popup_survey_ua_exclude', $form_state->getValue('popup_survey_ua_exclude'))
+      ->set('popup_survey_visibility_options', $form_state->getValue('popup_survey_visibility_options'))
+      ->set('popup_survey_visibility_options_pages', $form_state->getValue('popup_survey_visibility_options_pages'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+  protected function popupSurveyEntityOptions() {
+    $entities = $this->entity_manager->getStorage('popup_survey')->loadMultiple();
+    return array_combine($val = array_keys($entities), $val);
+  }
+
+  protected function popupSurveyFrequencyOptions() {
+    return array_combine($options = range(1, 100), $options);
+  }
+}
